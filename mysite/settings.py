d73da6ba5e4d642f3525ceefca4d869b8f4ad6c8
Abhit Patil <abhit95@gmail.com>
2021-07-30 21:01:37 +0200

    Link to certhelper DB for testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'database',
-    }
+        'ENGINE': config('DJANGO_DATABASE_ENGINE', default=''),
+        'NAME': config('DJANGO_DATABASE_NAME', default=''),
+        'USER': config('DJANGO_DATABASE_USER', default=''),
+        'PASSWORD': config('DJANGO_DATABASE_PASSWORD', default=''),
+        'HOST': config('DJANGO_DATABASE_HOST', default=''),
+        'PORT': config('DJANGO_DATABASE_PORT', default=''),
+    },
 }
 
 
