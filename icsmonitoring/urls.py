4e2a9aca13f825179a3a6def1bae178817277927
Abhit03 <abhit95@gmail.com>
2022-10-05 01:38:58 +0200

    Add tree view

--- icsmonitoring/urls.py
+++ icsmonitoring/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name='icsmonitoring'
 urlpatterns = [
     path("", views.icsmonitoringhome, name="icsmonitoringhome"),
+    path("plc", views.plcmonitoring, name="plcmonitoring"),
 ]
--- icsmonitoring/views.py
+++ icsmonitoring/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def icsmonitoringhome(request):
     return render(request, "icsmonitoring/home.html")
+
+def plcmonitoring(request):
+    return render(request, "icsmonitoring/localView.html")
